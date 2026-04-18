fa6c9bce8a28b0faaead5cb22d7a7065565f759d
Djordje Dimitrijev <dimitrijevdjordje1@gmail.com>
2026-04-18 23:39:33 +0200

    Add placeholder img and loading progress bar

--- app/src/main/java/com/app/realestatedemoapp/presentation/components/PropertiesLazyList.kt
+++ app/src/main/java/com/app/realestatedemoapp/presentation/components/PropertiesLazyList.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,6 +19,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Map
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -27,9 +29,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import coil.compose.AsyncImage
 import com.app.realestatedemoapp.R
@@ -42,82 +46,92 @@ fun PropertiesLazyList(
         isBookmarked: Boolean
     ) -> Unit
 ) {
-//    viewModel.updateBookmark(item.id, !item.isBookmarked
-    LazyColumn(
-        contentPadding = PaddingValues(10.dp)
-    ) {
-        items(
-            items = properties.itemSnapshotList.items, key = { property ->
-                property.id
-            }) { item ->
-            Card(
-                border = BorderStroke(2.dp, Color.Black),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-            ) {
-                Column {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(200.dp)
-                    ) {
-                        AsyncImage(
-                            model = item.imageUrl,
-                            contentDescription = stringResource(R.string.property_image),
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop
-                        )
+    if (properties.loadState.refresh == LoadState.Loading) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
+    } else {
+        LazyColumn(
+            contentPadding = PaddingValues(10.dp)
+        ) {
+            items(
+                items = properties.itemSnapshotList.items, key = { property ->
+                    property.id
+                }) { item ->
+                Card(
+                    border = BorderStroke(2.dp, Color.Black),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                ) {
 
-                        Text(
-                            text = item.getFormattedPrice(),
-                            color = Color.Black,
-                            fontWeight = FontWeight.Bold,
+                    Column {
+                        Box(
                             modifier = Modifier
-                                .align(Alignment.BottomStart)
-                                .padding(16.dp)
-                                .background(Color.White, RoundedCornerShape(8.dp))
-                                .padding(horizontal = 12.dp, vertical = 6.dp)
-                        )
-                        IconButton(
-                            onClick = { onBookmark(item.id, !item.isBookmarked) },
-                            modifier = Modifier
-                                .align(Alignment.TopEnd)
-                                .padding(horizontal = 6.dp, vertical = 6.dp)
+                                .fillMaxWidth()
+                                .height(200.dp)
                         ) {
-                            Icon(
-                                imageVector = if (item.isBookmarked) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                                tint = Color.Red,
-                                contentDescription = stringResource(R.string.favorites_icon)
+                            AsyncImage(
+                                placeholder = painterResource(R.drawable.placeholder),
+                                model = item.imageUrl,
+                                contentDescription = stringResource(R.string.property_image),
+                                modifier = Modifier.fillMaxSize(),
+                                contentScale = ContentScale.Crop
                             )
+
+                            Text(
+                                text = item.getFormattedPrice(),
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .align(Alignment.BottomStart)
+                                    .padding(16.dp)
+                                    .background(Color.White, RoundedCornerShape(8.dp))
+                                    .padding(horizontal = 12.dp, vertical = 6.dp)
+                            )
+                            IconButton(
+                                onClick = { onBookmark(item.id, !item.isBookmarked) },
+                                modifier = Modifier
+                                    .align(Alignment.TopEnd)
+                                    .padding(horizontal = 6.dp, vertical = 6.dp)
+                            ) {
+                                Icon(
+                                    imageVector = if (item.isBookmarked) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                                    tint = Color.Red,
+                                    contentDescription = stringResource(R.string.favorites_icon)
+                                )
+                            }
+
                         }
 
-                    }
-
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color.White)
-                            .padding(16.dp)
-                    ) {
-                        Text(
-                            text = item.title,
-                            style = MaterialTheme.typography.bodyLarge,
-                            color = Color.Black
-                        )
-                        Row {
-                            Icon(
-                                imageVector = Icons.Filled.Map,
-                                contentDescription = stringResource(R.string.location_icon),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.White)
+                                .padding(16.dp)
+                        ) {
                             Text(
-                                text = item.getFullAddress(),
+                                text = item.title,
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = Color.Black
                             )
+                            Row {
+                                Icon(
+                                    imageVector = Icons.Filled.Map,
+                                    contentDescription = stringResource(R.string.location_icon),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(
+                                    text = item.getFullAddress(),
+                                )
+                            }
                         }
                     }
                 }
             }
         }
     }
+
 }
